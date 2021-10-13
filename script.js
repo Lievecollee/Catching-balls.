dadce91b0d1855b17788a27055508b843fac9c4d
Lieve Collee <74552442+Lievecollee@users.noreply.github.com>
2021-10-13 12:43:57 +0000

    Ik heb ervoor gezorgd dat het geluid het doet als het spel begint. Ook heb ik georiënteerd geprogrammeerd.

--- script.js
+++ script.js
@@ -1,87 +1,166 @@
+function preload() {
+	soundFormats('m4a', 'mp3', 'ogg')
+	wind = loadSound("wind.mp3")
+}
+
 function setup() {
 	createCanvas(600, 400);
 	bb = loadImage("druppel.png");
 	achtergrond = loadImage("achtergrond.jpg")
 	emmer = loadImage("emmer.png")
-	soundFormats('m4a','mp3','ogg')
+	soundFormats('m4a', 'mp3', 'ogg')
 	druppelgeluid = loadSound("druppelgeluid.mp3")
-	wind = loadSound("wind.mp3")
+	player = new Emmer();
 }
 
 var screen = 0;
-var y=-20;
-var x=200;
+var y = -20;
+var x = 200;
 var speed = 2;
 var score = 0;
 var highscore = 0;
+var achtergrond;
+var druppels = [];
+
+class Druppel {
+	constructor(x, y, w, h) {
+		this.x = random(width);
+		this.y = 0;
+		this.w = 30;
+		this.h = 30;
+
+		if (score > 5) {
+			this.vy = 7;
+		}
+		if (score > 15) {
+			this.vy = 9;
+		}
+		else {
+			this.vy = 5;
+		}
+	}
+
+	draw() {
+		fill(0);
+		image(bb, this.x, this.y, this.w, this.h);
+		this.y += this.vy;
+
+	}
+
+	checkCollision() {
+		if (player.y < this.y) {
+			if (player.x + player.w > this.x && player.x < this.x + this.w) {
+
+				druppelgeluid.play()
+				score += 1
+				if (score > highscore);
+				highscore = score
+
+				let idx = druppels.indexOf(this);
+				druppels.splice(idx, 1);
+			}
+		}
+
+		if (this.y > height) {
+			screen = 2;
+		}
+	}
+
+}
+
+class Emmer {
+	constructor() {
+		this.x = 100;
+		this.y = height - 10;
+		this.w = 50;
+		this.h = 10;
+	}
+
+	draw() {
+		rectMode(CENTER)
+		this.x = mouseX;
+		rect(this.x, this.y, this.w, this.h);
+	}
+}
 
 function draw() {
-	if(screen == 0){
-    startScreen()
-  }else if(screen == 1){
-  	gameOn()
-  }else if(screen==2){
-  	endScreen()
-  }	
+	if (screen == 0) {
+		startScreen()
+	} else if (screen == 1) {
+		gameOn()
+	} else if (screen == 2) {
+		endScreen()
+	}
 }
 
-function startScreen(){
-		background("lightblue");
-		fill(255);
-		textAlign(CENTER);
-		text('Welkom bij "Catching Balls"!', width / 2, height / 2)
-		text('Klik op het scherm om het spel te beginnen', width / 2, height / 2 + 20);
-		reset();
+function startScreen() {
+	background("lightblue");
+	fill(255);
+	textAlign(CENTER);
+	text('Welkom bij "Catching Balls"!', width / 2, height / 2)
+	text('Klik op het scherm om het spel te beginnen', width / 2, height / 2 + 20);
+	reset();
 }
 
-function gameOn(){
-		background(achtergrond);
-		text("Score = " + score, 30,20);
-		text("Highscore = " + highscore, 42,40);
-		fill(255);
-  	image(bb,x,y,30,30);
-		rectMode(CENTER);
-  	image(emmer,mouseX,height-50,60,60);
-			y+= speed;
-  	if(y>height){
-  	screen = 2
-	 }
-	  if(y>height-10 && x>mouseX-30 && x<mouseX+30){
-  	y=-20
-    speed+=.5
-    score+= 1
-		druppelgeluid.play()
-		if (score>highscore);
-		 	highscore = score
-  }
-	if(y==-20){
-  	pickRandom();
-  }
+function gameOn() {
+	background(achtergrond);
+	fill(255);
+	text("Score = " + score, 30, 20,);
+	text("Highscore = " + highscore, 42, 40);
+	player.draw();
+
+	if (frameCount % 100 == 0) {
+
+		druppels.push(new Druppel());
+
+	}
+	if (frameCount % 100 == 50) {
+
+		druppels.push(new Druppel());
+	}
+
+	druppels.forEach((b) => {
+		b.draw();
+		b.checkCollision();
+	});
+
+	this.y += speed;
+	if (this.y > height) {
+		screen = 2
+	}
+
+	if (this.y == -20) {
+		pickRandom();
+	}
+
 }
 
-function pickRandom(){
-	x= random(20,width-20)
+function endScreen() {
+	background(150);
+	fill(250);
+	textAlign(CENTER);
+	text('GAME OVER', width / 2, height / 2);
+	text("Je score is = " + score, width / 2, height / 2 + 20);
+	text('Opnieuw proberen?', width / 2, height / 2 + 40);
 }
 
-function endScreen(){
-		background(150);
-		textAlign(CENTER);
-		text('GAME OVER', width / 2, height / 2);
-  	text("Je score is = " + score, width / 2, height / 2 + 20);
-		text('Opnieuw proberen?', width / 2, height / 2 + 40);
+function pickRandom() {
+	x = random(20, width - 20)
 }
 
-function mousePressed(){
-	if(screen==0){
-  	screen=1
+function mousePressed() {
+	if (screen == 0) {
+		screen = 1;
 		wind.play()
-  }else if(screen==2){
-  	screen=0
-  }
+	} 
+	else if (screen == 2) {
+		druppels = [];
+		screen = 0;
+	}
 }
 
-function reset(){
-	  score=0;
-  	speed=2;
-  	y=-20;
+function reset() {
+	score = 0;
+	speed = 2;
+	y = -20;
 }
